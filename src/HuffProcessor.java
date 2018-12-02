fdd996eb9eee5149ea4e3e62735e1daabd4459b9
Ashley Murray <ashley.j.murray@duke.edu>
2018-12-02 14:52:11 -0500

    anything works?

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -43,9 +43,9 @@ public class HuffProcessor {
 	public void compress(BitInputStream in, BitOutputStream out){
 
 		while (true){
-			int val = in.readBits(BITS_PER_WORD);
+			int val = in.readBits(BITS_PER_INT);
 			if (val == -1) break;
-			out.writeBits(BITS_PER_WORD, val);
+			out.writeBits(BITS_PER_INT, val);
 		}
 		out.close();
 	}
@@ -61,16 +61,18 @@ public class HuffProcessor {
 	public void decompress(BitInputStream in, BitOutputStream out){
 
 		while (true){
-			int val = in.readBits(BITS_PER_WORD);
+			int val = in.readBits(BITS_PER_INT);
 			if(val != HUFF_TREE) {
 				throw new HuffException("illegal header starts with "+val);
 			}
+			
 			if (val == -1) {
 				throw new HuffException("illegal header starts with "+ val);
 			}
+			
 			HuffNode root = readTreeHeader(in);
 			readCompressedBits(root, in, out);
-			out.writeBits(BITS_PER_WORD, val);
+			out.writeBits(BITS_PER_INT, val);
 			out.close();
 		}
 	}
